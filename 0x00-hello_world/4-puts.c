bd85b6e09db623450b4db37a82a96d70ec5a1ce6
mariam <mariammm3241@gmail.com>
2023-03-17 04:00:51 +0200

    task #4 - edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- *main: prints (Programming is like building a multilingual puzzle)
+ *main - prints (Programming is like building a multilingual puzzle)
  *using puts function
  *return: 0 (Success)
  */
