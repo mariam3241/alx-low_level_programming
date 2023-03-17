cf71b01877b16616f616514c55ecfc8a8642bef0
mariam <mariammm3241@gmail.com>
2023-03-17 03:40:51 +0200

    task #4 - edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- *The main function serves as the starting point for program execution
+ *The main prints (Programming is like building a multilingual puzzle) using puts function
  */
 int main(void)
 {
