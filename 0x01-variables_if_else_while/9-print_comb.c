ed7f59a838b78d476ac8299afe857ade9ff3ddd5
mariam <mariammm3241@gmail.com>
2023-03-21 01:01:05 +0200

    task #9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - a fun to print combinations of single gigits numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
