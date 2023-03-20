10bb016146184065415b8bc4f4a2006054b37c43
mariam <mariammm3241@gmail.com>
2023-03-21 00:21:38 +0200

    task #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - a fun to print single digit numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar('i');
+}
+putchar('\n');
+return (0);
+}
