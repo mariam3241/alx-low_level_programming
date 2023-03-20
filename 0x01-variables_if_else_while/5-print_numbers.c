f370a813021c2d09bdc8677ea348c169050fbf83
mariam <mariammm3241@gmail.com>
2023-03-20 23:52:18 +0200

    task #5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a fun to print numbers of single digits
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
