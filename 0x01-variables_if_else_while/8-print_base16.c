ba5260e209b80067f35eab3cc45de7b279add15c
mariam <mariammm3241@gmail.com>
2023-03-21 00:49:05 +0200

    task #8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - a fun to print numbers of base 16
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char str[100] = "0123456789abcdef";
+int i;
+for (i = 0 ; i < 15 ; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
