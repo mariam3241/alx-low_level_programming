e5aed5a49d710f2d7a96e758fc2e69f6364d93af
mariam <mariammm3241@gmail.com>
2023-03-21 01:54:32 +0200

    task #10 - 2nd edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ for (i = '0' ; i <= '8' ; i++)
 {
 for (j = '1' ; j <= '9' ; j++)
 {
-if (i != j && i < j)
+if (i != j && i < j && i != 8 && j != 9)
 {
 putchar(i);
 putchar(j);
@@ -21,5 +21,6 @@ putchar(' ');
 }
 }
 }
+putchar('\n');
 return (0);
 }
