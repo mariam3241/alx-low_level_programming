730538a721dede23d9cc0b9f6ff6ae2ffb8afc01
mariam <mariammm3241@gmail.com>
2023-03-20 22:09:03 +0200

    task #2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - a fun to print alphabet in lowercase
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+int i = 0;
+char str[100] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0 ; i < 26 ; i++)
+{
+putchar(str[i] + 32);
+}
+return (0);
+}
