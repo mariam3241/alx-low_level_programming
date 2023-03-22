d948cbbbd0158d18cdcd4bd2ad3f4c882775d6fa
mariam <mariammm3241@gmail.com>
2023-03-23 00:08:40 +0200

    task #1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - a fun to print alphabet in lowercase
+ * Return: Always (Success)
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
