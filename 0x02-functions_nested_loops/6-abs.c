19b8ec5ad16c4ddc138da07b3343c8d26287c4a5
mariam <mariammm3241@gmail.com>
2023-03-23 01:55:57 +0200

    task #6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - a fun that computes the absolute value
+ * @n: parameter
+ * Return: an int value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+int abs_val;
+abs_val = n * -1;
+return (abs_val);
+}
+return (n);
+}
