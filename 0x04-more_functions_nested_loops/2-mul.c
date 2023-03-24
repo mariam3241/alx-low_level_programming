23ba817a95d9f478d8210ea17acb60c7e0577405
mariam <mariammm3241@gmail.com>
2023-03-25 00:45:10 +0200

    task #2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - a fun tha multiplies 2 int
+ * @a: parameter of type int
+ * @b: 2nd parameter of type int
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
