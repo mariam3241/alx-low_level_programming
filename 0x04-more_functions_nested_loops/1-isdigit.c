3dfabb9a3eccea971ce222705f8f4d560b1812e9
mariam <mariammm3241@gmail.com>
2023-03-25 00:30:29 +0200

    task #1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - a fun to check digits
+ * @c: parameter
+ * Return: 1 if c is digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
