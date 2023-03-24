148e9ba3890187f5f5e6eab49038d37aca0e15e5
mariam <mariammm3241@gmail.com>
2023-03-25 00:57:01 +0200

    task #3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - a fun that prints numbers
+ *
+ * Return: Always (Success)
+ */
+void print_numbers(void)
+{
+int num;
+for (num = 48 ; num <= 57 ; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
