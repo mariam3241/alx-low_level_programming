3d767539a83cdd44a4186bba645328e1175d6fe5
mariam <mariammm3241@gmail.com>
2023-03-25 00:21:36 +0200

    task #0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ * _isupper - a fun to check the uppercase characters
+ * @c: parameter of type int
+ * Return: 1 if c is upeer and 0 othewise
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the README file for C-more functionsm more nested loops project
