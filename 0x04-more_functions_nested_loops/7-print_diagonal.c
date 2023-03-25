d31802092e19c722b49b57ee827b8c51e5d6b7f5
mariam <mariammm3241@gmail.com>
2023-03-25 03:25:33 +0200

    task #7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a fun that prints diagonal line on the screen
+ * @n: parameter
+ * Return: viod
+ */
+
+void print_diagonal(int n)
+{
+int i;
+if (i <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0 ; i < n ; i++)
+{
+_putchar('\');
+_putchar('\n');
+}
+}
+}
