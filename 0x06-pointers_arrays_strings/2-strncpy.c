7dd4585e28128ec55c9e6963d7d2eedfd68a575e
mariam <mariammm3241@gmail.com>
2023-03-31 23:00:32 +0200

    task #2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - a fun that copies a string
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: string value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (i < n && str[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
