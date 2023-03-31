6afd9771420124eb908a703bada2688df38f8056
mariam <mariammm3241@gmail.com>
2023-04-01 01:06:33 +0200

    task #7 - edited

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *c)
 int i, j;
 char s1[] = "aAeEoOtTlL";
 char s2[] = "4433007711";
-for (i = 0 ; c[i] != '\n' ; i++)
+for (i = 0 ; c[i] != '\0' ; i++)
 {
 for (j = 0 ; j < 10 ; j++)
 {
