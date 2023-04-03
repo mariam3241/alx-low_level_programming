38933fc44b2a301398c722a9b8ec65250fa472a5
mariam <mariammm3241@gmail.com>
2023-04-03 19:54:58 +0200

    task #0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a fun that fills memory with constatnt
+ * @s: parameter
+ * @b: parameter
+ * @n: parameter
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0 ; n > 0 ; i++)
+{
+s[i] = b;
+n__;
+}
+return (s);
+}
