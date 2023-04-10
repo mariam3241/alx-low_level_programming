264e74d646d645465d7085eb2480aac83616f41a
mariam <mariammm3241@gmail.com>
2023-04-10 22:18:33 +0200

    task #0 - 5 new file added

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - a fun that locates a substring
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+
+		while (*haystack == *needle && *needle != '\0')
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs -a fun that computes the absolute value of an integer
+ * @n: parameter
+ * Return: an integer value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
