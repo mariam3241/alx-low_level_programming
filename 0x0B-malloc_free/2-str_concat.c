66cfd698a9d1e1f49462fa138bec2bd394f79c05
mariam <mariammm3241@gmail.com>
2023-04-13 01:26:06 +0200

    task #2 - edited

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,6 +27,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s == NULL)
 		return (NULL);
+		i = j = 0;
 
 	while (s1[i] != '\0')
 	{
@@ -42,5 +43,4 @@ char *str_concat(char *s1, char *s2)
 
 	s[i] = '\0';
 	return (s);
-
 }
