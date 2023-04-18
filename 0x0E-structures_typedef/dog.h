e3e1fbcb676136f43fc857465de4fa9025e6c92d
mariam <mariammm3241@gmail.com>
2023-04-18 22:45:32 +0200

    file dog.h has been edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * struct dog - a new type defined by me!
  * @name: element
@@ -17,5 +14,8 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
