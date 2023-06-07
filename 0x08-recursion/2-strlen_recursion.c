810b7bdcac0ce466516c2ccb354ccff95f5977eb
mariam3241 <mariammm3241@gmail.com>
2023-06-07 19:22:01 +0300

    task #2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a fun that returns the length of a string
+ * @s: parameter
+ * Return: an intger value
+ */
+
+int _strlen_recursion(char *s)
+{
+int counter = 0;
+if (*s)
+{
+counter = counter + 1;
+counter + = _strlen_recursion(s + 1);
+}
+return (counter);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,4 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int is_prime_number(int n);
 int prime(int number, int i);
-
+int _strlen_recursion(char *s);
