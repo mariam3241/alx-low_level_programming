9c980d8f9998c3202b3e7841c9b18b18eae4f75e
mariam3241 <mariammm3241@gmail.com>
2023-06-10 16:36:50 +0300

    task #0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a fun that prints its name
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
