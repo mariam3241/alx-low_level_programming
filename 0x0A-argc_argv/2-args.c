2d4414df7aeb45f149d87e5ddd8dd74ad7bdc826
mariam3241 <mariammm3241@gmail.com>
2023-06-10 16:51:09 +0300

    task #2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a fun that prints all arguments it receives
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
