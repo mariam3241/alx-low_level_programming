b50a29f90bc0ce5b9f10b8df740e62a5f78a7ac3
mariam3241 <mariammm3241@gmail.com>
2023-06-23 19:40:49 +0300

    task #4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a fun that prints its own opcodes
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+		printf("%02hhx ", array[i]);
+	}
+	return (0);
+}
