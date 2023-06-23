37c338b5eb7fe073b99e8464925734019f72feba
mariam3241 <mariammm3241@gmail.com>
2023-06-23 16:13:41 +0300

    task #0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - a fun that prints a name
+ * @name: parameter
+ * @f: parameter
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+
+#endif
