ec65014002b7890e6a2601c1ac1624a538261539
mariam3241 <mariammm3241@gmail.com>
2023-06-30 11:39:29 +0300

    task #0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - a fun that prints all elements in list called list_t
+ * @h: parameter
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}
