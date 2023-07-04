45ee70a82c164f524f45c3292023d8d76561bec7
mariam3241 <mariammm3241@gmail.com>
2023-07-04 16:34:00 -0700

    task #8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * sum_listint - a fun that returns the sum of all data in a list
+ * @head: parameter
+ * Return: the sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
